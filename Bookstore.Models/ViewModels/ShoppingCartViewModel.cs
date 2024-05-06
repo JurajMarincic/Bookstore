14d1d04b1c48cd0b1d19091ef79c7f5c8d19bdd5
JurajMarincic <juraj.marincic1@gmail.com>
2024-05-06 16:43:09 +0200

    .

--- Bookstore.Models/ViewModels/ShoppingCartViewModel.cs
+++ Bookstore.Models/ViewModels/ShoppingCartViewModel.cs
@@ -13,5 +13,27 @@ namespace Bookstore.Models.ViewModels
         [ValidateNever]
         public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
         public double OrderTotal { get; set; }
+
+        public ShoppingCartViewModel(List<ShoppingCart> shoppingCarts)
+        {
+            double orderTotal = 0;
+            foreach (var cart in shoppingCarts)
+            {
+                if (cart.Count > 100)
+                {
+                    orderTotal += cart.Product.Price100 * cart.Count;
+                }
+                else if (cart.Count > 50)
+                {
+                    orderTotal += cart.Product.Price50 * cart.Count;
+                }
+                else
+                {
+                    orderTotal += cart.Product.Price * cart.Count;
+                }
+            }
+            OrderTotal = orderTotal;
+            ShoppingCartList = shoppingCarts;
+        }
     }
 }
--- Bookstore/Areas/Customer/Controllers/ShoppingCartController.cs
+++ Bookstore/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -2,23 +2,102 @@
 using Bookstore.DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
 using Bookstore.Models.ViewModels;
+using Bookstore.Models.Models;
 using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Bookstore.Areas.Customer.Controllers
 {
     [Area("Customer")]
-    public class ShoppingCartController : Controller
+	public class ShoppingCartController : Controller
     {
-        public ShoppingCartViewModel ShoppingCartViewModel { get; set; }
         private readonly IUnitOfWork _unitOfWork;
+
         public ShoppingCartController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
         }
+
         public IActionResult Index()
         {
-            
-            return View();
+            List<ShoppingCart> shoppingList = _unitOfWork.ShoppingCart.GetAll(includeProperties: "Product").ToList();
+            ShoppingCartViewModel shoppingCartView = new ShoppingCartViewModel(shoppingList);
+            return View(shoppingCartView);
         }
+
+        public int GetShoppingCartItemCount()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCart.GetAll(includeProperties: "Product").ToList();
+            int count = 0;
+            foreach (var cart in shoppingCarts)
+            {
+                if (cart.ApplicationUserId == userId)
+                {
+                    count += cart.Count;
+                }
+            }
+            return count;
+        }
+
+
+
+        public IActionResult Delete(int? shoppingCartId)
+        {
+            ShoppingCart? shoppingCart = _unitOfWork.ShoppingCart.Get(c => c.Id == shoppingCartId);
+
+            if (shoppingCart == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.ShoppingCart.Remove(shoppingCart);
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult AddOneItem(int? shoppingCartId)
+        {
+            ShoppingCart? shoppingCart = _unitOfWork.ShoppingCart.Get(c => c.Id == shoppingCartId);
+
+            if (shoppingCart == null)
+            {
+                return NotFound();
+            }
+            shoppingCart.Count++;
+            _unitOfWork.ShoppingCart.Update(shoppingCart);
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult DeleteOneItem(int? shoppingCartId)
+        {
+            ShoppingCart? shoppingCart = _unitOfWork.ShoppingCart.Get(c => c.Id == shoppingCartId);
+
+            if (shoppingCart == null)
+            {
+                return NotFound();
+            }
+
+            if (shoppingCart.Count == 1)
+            {
+                _unitOfWork.ShoppingCart.Remove(shoppingCart);
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+
+            }
+            shoppingCart.Count--;
+            _unitOfWork.ShoppingCart.Update(shoppingCart);
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Summary()
+        {
+            List<ShoppingCart> shoppingList = _unitOfWork.ShoppingCart.GetAll(includeProperties: "Product").ToList();
+            ShoppingCartViewModel shoppingCartView = new ShoppingCartViewModel(shoppingList);
+            return View(shoppingCartView);
+        }
+
     }
 }
